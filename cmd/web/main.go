a80f9594961272e1937505e97ed05d91d2b9c5a2
Yasen El Makioui <workspaceyasen@gmail.com>
2025-11-17 21:22:16 +0100

    Added viper for the configuration management and added directly the config on main for testing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"revaultier/database"
 	"revaultier/internal/auth"
 	"revaultier/internal/root"
 	"revaultier/internal/server"
 	"revaultier/internal/user"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	database := database.NewDatabase("./test.db")
+
+	// Get config file. Move this in internal to be retrieved by main
+	viper.SetConfigFile("revaultier.toml")
+	viper.AddConfigPath("$HOME/.config/revaultier")
+	viper.AddConfigPath("/etc/revaultier")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatal(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	secretkey := []byte(viper.GetString("auth.secretkey"))
+	databaseName := viper.GetString("database.database")
+
+	if string(secretkey) == "" {
+		log.Fatal(fmt.Errorf("set the secretkey in the configuration file"))
+	}
+
+	database := database.NewDatabase(databaseName)
 
 	rootHandler := root.NewRootHandler()
 
@@ -18,7 +41,7 @@ func main() {
 	userHandler := user.NewUserHandler(userService)
 
 	authRepository := auth.NewAuthRepository(database)
-	authService := auth.NewAuthService(authRepository, []byte("secretkey")) // temp, will be taken from config
+	authService := auth.NewAuthService(authRepository, secretkey) // temp, will be taken from config
 	authHandler := auth.NewAuthHandler(authService)
 
 	e := server.NewServer([]byte("secretkey"), rootHandler, userHandler, authHandler)
